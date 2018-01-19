81c777290c0cd93e1962b2d342971e42180d77cc
Unknown <filtcutoff@gmail.com>
2018-01-19 12:47:53 +0800

    Added nn-output-interpreter file in /nnmaster (No contents yet)

---
+++ nnmaster/moira.py
@@ -0,0 +1,9 @@
+'''
+MOIRA
+Mediating Output Interpreter for Real-time Analysis
+version 1.0
+author: R2-B09, 2018
+
+An algorithm that shows the chord name
+for every combination of neural network outputs.
+'''
