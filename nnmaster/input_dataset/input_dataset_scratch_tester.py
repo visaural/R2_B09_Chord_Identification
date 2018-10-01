09af2f7d3e4b05739419748f37391af3f8c26c5f
Unknown <filtcutoff@gmail.com>
2018-10-01 19:44:53 +0800

    Excel latencies finished; py files updated with latency testing

--- nnmaster/input_dataset/input_dataset_scratch_tester.py
+++ nnmaster/input_dataset/input_dataset_scratch_tester.py
@@ -4,20 +4,22 @@ import random
 TEST_FILE = "chord_master_list.txt"
 
 def get_line_index_from_file(f, i):
-    return linecache.getline(f, i + 1)
+    return linecache.getline(f, i)
 
-def random_int(n, LB, UB):
-    if n == 0:
+def random_ints_in_range(num_samples, LB, UB):
+    if num_samples == 0:
         return None
-    elif n == 1:
+    elif num_samples == 1:
         return random.randint(LB, UB)
     else:
-        l = [i for i in range(n)]
-        k = random.sample(l)
-        return k
+        l = [i for i in range(LB, UB + 1)]
+        k = random.sample(l, num_samples)
+
+    return k
 
 if __name__ == "__main__":
     #print(linecache.getline(TEST_FILE, 3))
     #print(get_line_index_from_file(TEST_FILE, 2))
 
-    random_int(30, 1, 49)
+    #print(random_ints_in_range(30, 1, 444))
+    print(get_line_index_from_file("input_dataset.txt", 5))
